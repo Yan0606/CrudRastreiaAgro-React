4b560926595d65a8acf0491798f026996d8e7aa8
yan0606 <yan060604@gmail.com>
2024-08-16 09:31:05 -0300

    init: 🎉 iniciar repositório de estudos sobre React com foco em CRUDs

---
+++ src/App.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import { Container, Row, Col, Card, Form, Button } from 'react-bootstrap';
+import { NavLink, Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import Home from './components/Home';
+
+function Login() {
+  return (
+    <Container className="mt-5">
+      <Row className="justify-content-center">
+        <Col md={6}>
+          <Card>
+            <Card.Header>Acesso - Sistema BookSell</Card.Header>
+            <Card.Body>
+              <Form>
+                <Form.Group className="mb-3" controlId="email">
+                  <Form.Label>Email</Form.Label>
+                  <Form.Control type="email" placeholder="Digite seu email" />
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="senha">
+                  <Form.Label>Senha</Form.Label>
+                  <Form.Control type="password" placeholder="Digite sua senha" />
+                </Form.Group>
+
+                <div className="mb-3">
+                  <p>
+                    Ainda não tem conta? Clique <NavLink to="/cadastrar">aqui</NavLink> para se cadastrar!
+                  </p>
+                </div>
+
+                <NavLink to="/components/Home">
+                  <Button variant="primary">Acessar</Button>
+                </NavLink>
+              </Form>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
+  );
+}
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/components/Home" element={<Home />} />
+        {/* Outras rotas podem ser adicionadas aqui */}
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
